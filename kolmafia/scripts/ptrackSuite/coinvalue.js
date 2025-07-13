b9448e4039fa9eccb3cac5d2c53076191ffd2dca
Prusias-kol <121605604+Prusias-kol@users.noreply.github.com>
2025-07-12 18:40:00 -0700

    remove decimals

--- kolmafia/scripts/ptrackSuite/coinvalue.js
+++ kolmafia/scripts/ptrackSuite/coinvalue.js
@@ -5,12 +5,11 @@ function addCommas(nStr) {
     nStr += '';
     var x = nStr.split('.');
     var x1 = x[0];
-    var x2 = x.length > 1 ? '.' + x[1].substring(0,3) : '';
     var rgx = /(\d+)(\d{3})/;
     while (rgx.test(x1)) {
             x1 = x1.replace(rgx, '$1' + ',' + '$2');
     }
-    return x1 + x2;
+    return x1;
 }
 
 groupedMap = new Map();
